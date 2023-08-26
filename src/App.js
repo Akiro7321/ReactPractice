1beb7340c4bed3cea76297bbaadf9811ddaa2b08
nicholas nicholas <suijinkappa@gmail.com>
2023-08-26 13:43:10 +0900

    checking if website will update

--- src/App.js
+++ src/App.js
@@ -4,30 +4,36 @@ import './App.css';
 import { useState } from 'react';
 import './App.css'
 
+
 const App = () =>{
+  let yenValue = 1;
 	
 	return (
-    <CounterComponent />
+    <div className="App">
+      <CounterComponent yenValueProp={yenValue}/>
+    </div>
+    
 	)
 }
 
-const CounterComponent = () =>{
+const CounterComponent = ({yenValueProp}) =>{
   const [counter, setCounter] = useState(0);
 
   return(
-    <div className="App">
-      <div className="CounterApp">
-        <h1>NUMBER COUNTING</h1>
-			  <h2>{counter}</h2>
-        <div className="Buttons">
-          <button className="button" onClick={() => setCounter((prevCount) => prevCount - 1)}>-</button>
-          <button className="button" onClick={() => setCounter((prevCount) => prevCount + 1)}>+</button>
-          <button className="button" onClick={() => setCounter(0)}>Reset</button>
+      <div>
+        <div className="CounterApp">
+          <h1>Bank account</h1>
+          <h2>{counter} ¥</h2>
+          <h2>current yen value {yenValueProp} ¥</h2>
+          <h2>current yen value {yenValueProp} ¥</h2>
+
+          <div className="Buttons">
+            <button className="button" onClick={() => setCounter((prevCount) => prevCount + yenValueProp)}>+</button>
+          </div>
         </div>
       </div>
-			
-		</div>
   )
+  
 }
 
 
